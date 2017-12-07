bbd4eecd6e6867a723d442135781e92cfce9ea78
Bengt Marten Agren <maraingist@me.com>
2017-12-07 21:57:05 +0800

    Reinstated port discovery on Windows (need Linux version later).

--- src/Internal/USBSerial.hs
+++ src/Internal/USBSerial.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE RecordWildCards #-}
 module Internal.USBSerial (USBSerial(..), findPort, usbSerials) where
 
---import System.Win32.Registry (hKEY_LOCAL_MACHINE, regOpenKey, regCloseKey, regQueryValue, regQueryValueEx)
---import System.Win32.Types (DWORD, HKEY)
+import System.Win32.Registry (hKEY_LOCAL_MACHINE, regOpenKey, regCloseKey, regQueryValue, regQueryValueEx)
+import System.Win32.Types (DWORD, HKEY)
 import Control.Exception (handle, bracket, SomeException(..))
 import Foreign (toBool, Storable(peek, sizeOf), castPtr, alloca) 
 import Data.List.Split (splitOn)
@@ -27,7 +27,26 @@ findPort :: Int -> Int -> IO (Maybe String)
 findPort vendorId productId = fmap (fmap portName . listToMaybe) $ usbSerials (Just vendorId) (Just productId)
 
 usbSerials :: Maybe Int -> Maybe Int -> IO [USBSerial]
-usbSerials mVendorId mProductId = undefined
+usbSerials mVendorId mProductId = withHKey hKEY_LOCAL_MACHINE path $ \hkey -> do
+    n <- fmap fromEnum $ regQueryValueDWORD hkey "Count"
+    fmap catMaybes $ forM [0..n-1] $ \i -> do
+        key <- regQueryValue hkey . Just . show $ i
+        case keyToVidPid key of
+            Just (vendorId, productId)
+                | maybe True (==vendorId) mVendorId && maybe True (==productId) mProductId -> do
+                    portName <- getPortName key
+                    friendlyName <- getFriendlyName key
+                    return $ Just USBSerial{..}
+            _ -> return Nothing
+    where path = "SYSTEM\\CurrentControlSet\\Services\\usbser\\Enum"
+
+getPortName :: String -> IO String
+getPortName serial = withHKey hKEY_LOCAL_MACHINE path $ flip regQueryValue (Just "PortName")
+    where path = "SYSTEM\\CurrentControlSet\\Enum\\" ++ serial ++ "\\Device Parameters"
+
+getFriendlyName :: String -> IO String
+getFriendlyName serial = withHKey hKEY_LOCAL_MACHINE path $ flip regQueryValue (Just "FriendlyName")
+    where path = "SYSTEM\\CurrentControlSet\\Enum\\" ++ serial
 
 keyToVidPid :: String -> Maybe (Int, Int)
 keyToVidPid name
@@ -39,4 +58,16 @@ keyToVidPid name
     where fromHex s = case readHex s of
             [(x, "")] -> Just x
             _         -> Nothing
+
+withHKey :: HKEY -> String -> (HKEY -> IO a) -> IO a
+withHKey hive path
+    = handle (\(SomeException e) -> error $ show e ++ ": " ++ path)
+    . bracket (regOpenKey hive path) regCloseKey
+
+-- | Read DWORD value from registry.
+-- From http://compgroups.net/comp.lang.haskell/working-with-the-registry-windows-xp/2579164
+regQueryValueDWORD :: HKEY -> String -> IO DWORD
+regQueryValueDWORD hkey name = alloca $ \ptr -> do
+    regQueryValueEx hkey name (castPtr ptr) (sizeOf (undefined :: DWORD))
+    peek ptr
 
