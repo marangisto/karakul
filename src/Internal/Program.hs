f5bd344470f66386f8a85fa35bf5fcd82ceb8df4
Bengt Marten Agren <marangisto@me.com>
2019-07-16 17:31:57 +0800

    Used STM32 programmer for STM32F411.

--- src/Internal/Program.hs
+++ src/Internal/Program.hs
@@ -24,10 +24,13 @@ programmer Nothing STM32F103 bin = return
         , "0x8000000"
         ])
 programmer Nothing STM32F411 bin = return
-    ("st-flash", \_ ->
-        [ "write"
+    ("STM32_Programmer_CLI", \_ ->
+        [ "--connect port=SWD mode=UR"
+        , "--write"
         , bin
         , "0x8000000"
+        , "--verify"
+        , "-hardRst"
         ])
 programmer Nothing STM32F412 bin = return
     ("st-flash", \_ ->
